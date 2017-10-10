187d189594ab02410b31c64a59f9b02eeef89d78
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2017-10-10 18:41:50 +0200

    add config

---
+++ config/__init__.py
@@ -0,0 +1,14 @@
+import json
+import os
+from os.path import join
+from os.path import expanduser as expnd
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+
+with open(join(cwd, 'config.json')) as json_file:
+    config = json.load(json_file)
+
+for key in config.keys():
+    config[key] = expnd(config[key])
+
+vars().update(config)
