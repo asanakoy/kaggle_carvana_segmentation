ec4011b591fe1e89bf7865ffc4c8ada2027b43b6
Alexander Buslaev <a.buslaev@geoscan.lan>
2017-10-13 18:20:25 +0300

    check and install conda fix versions for albu env fix torchvision for asanakoy

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+which conda
+if [ "$?" -eq 1 ]; then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+    chmod +x Miniconda2-latest-Linux-x86_64.sh
+    ./Miniconda2-latest-Linux-x86_64.sh
+else
+    echo "Miniconda already installed"
+fi
+
 conda create -n py35_albu python=3.5
 source activate py35_albu
 
